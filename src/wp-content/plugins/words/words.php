3878720f423f40f37c1ddbe3dad2112307695478
RubenDanielCastelao <rnunezgonzalez@danielcastelao.org>
2023-11-29 09:29:01 +0100

    Plugin DataBase

--- src/wp-content/plugins/words/words.php
+++ src/wp-content/plugins/words/words.php
@@ -1,37 +1,65 @@
 <?php
 
 /*
-Plugin Name: Word Randomizer
+Plugin Name: Word DB Replacer
 Plugin URI: http://wordpress.org/plugins/words/
-Description: This plugin replaces some words with other words.
+Description: This plugin replaces some words with other words using access to DataBase.
 Author: Rubén Núñez
 Version: 3.3
 Author URI: https://ma.tt/
 */
 
-function renym_wordpress_typo_fix($text)
-{
-    //Create a 5 length array of cool places name
-    $places = array(
-        'Buenos Aires',
-        'Córdoba',
-        'La Plata',
-        'Mar del Plata',
-        'Mendoza');
+function myplugin_update_db_check() {
+    global $wpdb;
 
-    //Randomize the order of the places array
-    shuffle($places);
+    $charset_collate = $wpdb->get_charset_collate();
 
-    //Create another 5 word length array of cool cars
-    $cars = array(
-        'Ford',
-        'Chevrolet',
-        'Fiat',
-        'Renault',
-        'Peugeot');
+    $table_name = $wpdb->prefix . 'damRuben';
 
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        name tinytext NOT NULL,
+        text text NOT NULL,
+        url varchar(55) DEFAULT '' NOT NULL,
+        PRIMARY KEY (id)
+    )$charset_collate;";
 
-    return str_replace($places, $cars, $text);
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    $name='Rubén';
+    $text='Prueba de inserción con DB';
+
+    $result = $wpdb->insert(
+        $table_name,
+        array(
+            'time' => current_time( 'mysql' ),
+            'name' => $name,
+            'text' => $text,
+        )
+    );
+
+    error_log("Comprobar inserción: " . $result);
 }
 
-add_filter('the_content', 'renym_wordpress_typo_fix');
+add_action( 'plugins_loaded', 'myplugin_update_db_check' );
+
+
+function renym_wordpress_typo_fix( $text ) {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'damRuben';
+
+    $resultado = $wpdb->get_results("SELECT * FROM " . $table_name, ARRAY_A);
+
+    foreach($resultado as $fila)
+    {
+        error_log("Recorremos resultado: " . $fila['time']);
+    }
+
+    return str_replace( 'WordPressPlugin', $resultado , $text );
+}
+
+add_filter( 'the_content', 'renym_wordpress_typo_fix' );
+
